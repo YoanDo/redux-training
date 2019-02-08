e4914e5939e149e19b18e535b7d4195e63c8400a
Y <y.n.dorny@gmail.com>
2019-02-08 13:54:52 +0100

    Setting up React Router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,7 +3,21 @@ import { render } from 'react-dom';
 // css
 import css from './styles/style.styl';
 // import component
-import Main from './components/Main'
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-render(<Main/>, document.getElementById('root'));
+// Our main component brings to the photogrid elements as it's the one we want to display by default
+const router = (
+  <Router history= {browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>)
+
+// instead of rendering <Main/> like in a single page app we render the router above
+render(router, document.getElementById('root'));
 
