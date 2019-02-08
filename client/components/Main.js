7b73ee470a928222b720c4bf7ae99ff58a09b274
Y <y.n.dorny@gmail.com>
2019-02-08 13:33:17 +0100

    Creating Single and PhotoGrid components + add prettier

--- client/components/Main.js
+++ client/components/Main.js
@@ -5,12 +5,13 @@ const Main = React.createClass({
   render() {
     return (
       <div className="">
-        <Link href="/">
-          <h1>Reduxstagram</h1>
-          </Link>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
-      )
+    );
   }
-})
+});
 
 export default Main;
