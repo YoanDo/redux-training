e440031773ff1062eeeba29077c7517fdffc4ece
Y <y.n.dorny@gmail.com>
2019-02-11 18:51:42 +0100

    Single photo component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './photo';
+import Comments from './comments';
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">single</div>;
+    // index of the post
+    // here I'm looking for the post that has as an id the url of the page single page > this.props.params.postId
+    // get the post
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    return (
+      <div className="single-photo">
+        <Photo index={i} post={post} {...this.props} />;
+        <Comments />
+      </div>
+    );
   }
 });
 
