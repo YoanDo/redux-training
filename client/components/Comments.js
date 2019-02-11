5358cd35e25eb3f41c3cad63ac661dc2262d5e87
Y <y.n.dorny@gmail.com>
2019-02-11 19:26:44 +0100

    display and hide comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+        </p>
+        <button className="remove-comment">&times;</button>
+      </div>
+    );
+  },
   render() {
-    return (<div className="comment">I'm a comment</div>);
+    const { postComments } = this.props;
+    return (
+      <div className="comments">
+        {postComments.map(this.renderComment)}
+        <form action="" ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          {/* input hidden i necessary to let the user hit enter when he is done */}
+          <input type="submit" hidden />
+        </form>
+      </div>
+    );
   }
 });
 
--- client/components/Single.js
+++ client/components/Single.js
@@ -8,14 +8,14 @@ const Single = React.createClass({
     // index of the post
     // here I'm looking for the post that has as an id the url of the page single page > this.props.params.postId
     // get the post
-    const i = this.props.posts.findIndex(
-      post => post.code === this.props.params.postId
-    );
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex(post => post.code === postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
         <Photo index={i} post={post} {...this.props} />;
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     );
   }
